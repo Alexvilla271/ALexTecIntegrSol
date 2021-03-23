4feb528efc09151d6890f29c9f3c57c9d55958e3
Alexvilla271 <rayo_campeon_44@hotmail.com>
2021-03-22 20:42:45 -0600

    Aceptar cualquier valor

--- Práctica4(dosMas)/cliente-soap/soap.js
+++ Práctica4(dosMas)/cliente-soap/soap.js
@@ -2,8 +2,8 @@ var mensaje = '<? xml version = "1.0" encoding = "utf-8" ?>' +
     '<Envelope xmlns="http://schemas.xslsoap.org/soap/envelope/">' +
     '<Body>' +
     '<SumarRequest xmlns= "http://www.example.org/calculadora">' +
-    '<a>4</a>' + //Modificar esto ppara que acepte cualquier valor
-    '<b>4</b>' + //Modificar esto ppara que acepte cualquier valor
+    '<a>4</a>' + //No supe cómo hacer para que acepte cualquier valor:(
+    '<b>4</b>' + //No supe cómo hacer para que acepte cualquier valor:(
     '</SumarRequest>' +
     '</Body>' +
     '</Envelope>';
