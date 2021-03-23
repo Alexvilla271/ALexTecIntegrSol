9e40b3b33c6aa5e94e56bda68fb6f4a77556c597
Alexvilla271 <rayo_campeon_44@hotmail.com>
2021-03-22 20:31:50 -0600

    Primer commit

--- Práctica3(copiado)/demo/src/main/java/com/example/demo/calculadoraEndPoint.java
+++ Práctica3(copiado)/demo/src/main/java/com/example/demo/calculadoraEndPoint.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.example.calculadora.SumarRequest;
 import org.example.calculadora.SumarResponse;
-import org.example.calculadora.RestartRequest;
-import org.example.calculadora.RestartResponse;
-import org.example.calculadora.DiviRequest;
-import org.example.calculadora.DiviResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -15,7 +11,6 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class calculadoraEndPoint {
 
     @PayloadRoot(namespace = "http://www.example.org/calculadora", localPart = "SumarRequest")
-    @PayloadRoot(namespace = "http://www.example.org/calculadora", localPart = "RestarRequest")
 
     @ResponsePayload
     public SumarResponse dameSuma(@RequestPayload SumarRequest peticion){
@@ -27,27 +22,4 @@ public class calculadoraEndPoint {
 
         return respuesta;
     }
-
-    @ResponsePayload
-    public RestartResponse dameSuma(@RequestPayload RestarRequest peticion){
-        RestarRequest respuesta= new RestartResponse();
-        respuesta.setResultado(String.valueOf(
-            Integer.parseInt( peticion.getA() ) +
-            Integer.parseInt( peticion.getB() )
-        ));
-
-        return respuesta;
-    }
-
-    @ResponsePayload
-    public DiviResponse dameSuma(@RequestPayload DiviRequest peticion){
-        DiviRequest respuesta= new DiviResponse();
-        respuesta.setResultado(String.valueOf(
-            Integer.parseInt( peticion.getA() ) +
-            Integer.parseInt( peticion.getB() )
-        ));
-
-        return respuesta;
-    }
-
 }
